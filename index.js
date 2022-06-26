c366dcae74b7b1a3d21f9cadb1ccf18736942615
Jacob Janak <jakeathotmail@gmail.com>
2022-06-26 14:08:49 -0400

    Updated console logs

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const { spawn } = require('child_process');
 const buildCmd = spawn('npm', ['run', 'build']);
 buildCmd.stdout.on('data', data => console.log(data.toString()));
 buildCmd.stderr.on('data', data => console.log(data.toString()));
-buildCmd.on('error', err => handleError(err));
+buildCmd.on('error', err => handleErr(err));
 buildCmd.on('exit', exitCode => {
-  if (exitCode !== 0) handleError();
+  if (exitCode !== 0) handleErr();
 
   // Read the compiled index.html file from the /dist directory
   fs.readFile('./dist/index.html', 'utf-8', function(err, data) {
-    if (err) handleError(err);
+    if (err) handleErr(err);
 
     // Use Regex to make file imports relative to index.html
     const cssRe = /(href=")\/.*?\//g;
@@ -23,7 +23,7 @@ buildCmd.on('exit', exitCode => {
 
     // Update HTML
     fs.writeFile('./dist/index.html', result, 'utf-8', function(err) {
-      if (err) handleError(err);
+      if (err) handleErr(err);
 
       // Rename /dist to /docs because gh-pages can be set to look for /docs
       const src = path.join(__dirname, 'dist');
@@ -41,38 +41,41 @@ buildCmd.on('exit', exitCode => {
 });
 
 
-// Prints error messages to console
-function handleError(err) {
+// Prints error and help messages to the console.
+function handleErr(err, msgOnly) {
   if (!err) err = "";
   else err += "\n";
-  err += "\nFor help using vue-easy-deploy, please visit:\n\x1b[36mhttps://github.com/jacobjanak/vue-easy-deploy/blob/main/README.md\x1b[0m\n";
-  throw err;
+  err += `\nFor${msgOnly ? ' more' : ''} help using vue-easy-deploy, please visit:\n\x1b[36mhttps://github.com/jacobjanak/vue-easy-deploy/blob/main/README.md\x1b[0m\n`;
+  if (msgOnly) console.log(err);
+  else throw err;
 }
 
 
 // Prints success message to console
 function success() {
-  console.log(`
-Ready to deploy!
+  handleErr(`
+\x1b[32mReady to deploy to GitHub pages!\x1b[0m
 
-Next steps:
+\x1b[1mNext steps:\x1b[0m
                 
-  1. git add, commit, and push as you normally would.
+  1. Simply git add, commit, and push as you normally would.
 
-If this is your first time deploying this repo to GitHub pages:
+
+\x1b[1mIf this is your first time deploying this repo:\x1b[0m
                 
-  2. go to the pages settings of your repo and set it to deploy from the /docs folder.
-    https://github.com/<USER NAME>/<REPO NAME>/settings/pages
-  `);
+  2. Go to your GitHub pages settings (replace values between brackets):
+  \x1b[36mhttps://github.com/[USER NAME]/[REPO NAME]/settings/pages\x1b[0m
+
+  3. Set GitHub pages to deploy from the /docs folder.\n`, true);
 }
 
 
 // Read all files in a directory and call copyAllFiles
 function copyDir(src, dest, cb=false) {
   fs.mkdir(dest, { recursive: true }, err => {
-    if (err) handleError(err);
+    if (err) handleErr(err);
     fs.readdir(src, { withFileTypes: true }, (err, files) => {
-      if (err) handleError(err);
+      if (err) handleErr(err);
       copyAllFiles(src, dest, files, 0, cb);
     });
   });
@@ -90,7 +93,7 @@ function copyAllFiles(src, dest, files, i, cb=false) {
     });
   } else {
     fs.copyFile(srcPath, destPath, err => {
-      if (err) handleError(err);
+      if (err) handleErr(err);
       if (i === files.length - 1) cb();
       else copyAllFiles(src, dest, files, i+1, cb);
     });
